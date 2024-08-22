c45bd638323121c4cd33a04ed99c1b867300f689
csseifms <csseifms@gmail.com>
2024-08-22 21:49:42 +0300

    adding a class, knowing what is required

--- calculator.py
+++ calculator.py
@@ -14,12 +14,30 @@ def divide(x, y):
     return x / y
 
 
-def square(x):
-    return x * x
+
+def calculator(operator):
+    x = int(input("enter first number "))
+    y = int(input("enter second number "))
+    return operator(x, y)
 
 
 def greet(x):
-    print(f"Hello,{x}")
+    print(f"Hello,{x}! Welcome to our calculator!")
+    op = input("choose operator: (add/sub/multiply/divide/square) or off for shutdown ")
+    while op != "off":
+        if op == "add":
+            print(calculator(add))
+        elif op == "subtract":
+            print(calculator(subtract))
+        elif op == "multiply":
+            print(calculator(multiply))
+        elif op == "divide":
+            print(calculator(divide))
+        else:
+            print("invalid operator")
+
+        op = input("another operation ? "
+                   "choose operator: (add/sub/multiply/divide) or off for shutdown ")
 
 
 def username():
---
+++ csseifms.py
@@ -0,0 +1,9 @@
+class Csseifms:
+    def __init__(self):
+        print("welcome to my platform"
+              "here you will find CV"
+              "Learning Section"
+              "some")
+        print("https://www.notion.so/techclub/CSSEIFMS-dbe44477bb66433cb740b1165e2c2941?pvs=4")
+        pass
+
